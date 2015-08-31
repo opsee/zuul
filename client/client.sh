51938d59dca4522e96ebe339affb58f4708a894a
Greg Poirier <grep@binary-snobbery.com>
2015-08-31 10:55:45 -0700

    Don't set default username/password in client.sh

--- client/client.sh
+++ client/client.sh
@@ -3,8 +3,8 @@
 . /zuul/common/common.sh
 
 cat <<EOF >/dev/shm/up
-${CUSTOMER_ID:-greg}
-${VPN_PASSWORD:-crvo8u4B1Q1apn}
+${BASTION_ID}
+${VPN_PASSWORD}
 EOF
 chown zuul:zuul /dev/shm/up
 chmod 600 /dev/shm/up
